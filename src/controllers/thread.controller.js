18aa9cf1190c622c06fd310d7520084faf0ff8cd
arifaeva <arifa.eva.ixa@gmail.com>
2024-05-12 22:04:45 +0700

    update assignment

--- src/controllers/thread.controller.js
+++ src/controllers/thread.controller.js
@@ -1,27 +1,13 @@
 const Thread = require("../models/thread.model.js");
 const Session = require("../models/session.model.js");
 
-const threads = [];
-
-// JWT
+// Session
 async function handleGetAllThreadsSession(req, res) {
-  const { title, content } = req.body;
-
-  const sessionId = req.cookies.session_id;
-  // console.log(sessionId);
-  const session = await Session.findById(sessionId);
-
-  const newThread = new Thread({
-    title,
-    content,
-    userId: session.userId,
-  });
-
-  const savedThread = await newThread.save();
+  const allThreads = await Thread.find();
 
   return res
     .status(200)
-    .json({ message: "Successfully get all threads!", data: threads });
+    .json({ message: "Successfully get all threads!", data: allThreads });
 }
 
 async function handlePostNewThreadSession(req, res) {
@@ -38,7 +24,6 @@ async function handlePostNewThreadSession(req, res) {
   });
 
   const savedThread = await newThread.save();
-  threads.push(savedThread);
 
   res.status(201).json({
     message: "Successfully post a new thread!",
--- src/server.js
+++ src/server.js
@@ -11,9 +11,6 @@ const MONGO_DB_URL = require("./config/dburl.config.js");
 const app = express();
 
 mongoose.connect(MONGO_DB_URL);
-// mongoose.connect(
-//   "mongodb+srv://arifaevaixa:LEGg7UJbMdWdNV1i@devscaleid.diepraq.mongodb.net/?retryWrites=true&w=majority&appName=DevscaleID"
-// );
 
 app.use(express.json());
 app.use(cookieParser());
